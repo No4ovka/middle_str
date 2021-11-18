cb848690a6c004452e3f2294d217910125d8136d
No4ovka <92082996+No4ovka@users.noreply.github.com>
2021-11-18 21:06:50 +0300

    Update to_int.cpp

--- to_int.cpp
+++ to_int.cpp
@@ -3,25 +3,21 @@
 string itc_ToString(int num){
     string res = "", result = "";
     int point;
-    if(num == 0){
+    if(num == 0)
         return "0";
-    }
     while(num > 0){
         point = num % 10;
         res += '0' + point;
         num = num / 10;
-    }
-    for(int i = itc_len(res) - 1; i >= 0; i--){
+    for(int i = itc_len(res) - 1; i >= 0; i--)
         result += res[i];
-    }
     return result;
 }
 long long itc_ToInt(string str){
     long long num = 0;
     for(int i = 0; i < itc_len(str); i++){
-        if(str[i] >= '0' and str[i] <= '9'){
+        if(str[i] >= '0' and str[i] <= '9')
             num = num * 10 + (str[i] - '0');
-        }
     }
     return num;
 }
