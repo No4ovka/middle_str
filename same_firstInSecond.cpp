ffe9303198f8605b8d0f7daccc8274fdd2fa277f
No4ovka <92082996+No4ovka@users.noreply.github.com>
2021-11-19 18:07:43 +0300

    Add files via upload

--- same_firstInSecond.cpp
+++ same_firstInSecond.cpp
@@ -4,19 +4,12 @@ char itc_sameChar(string str){
     long long a = itc_len(str), b = 0;;
     for (int k = 0; k < a; k++){
         for (int i = 0; i < a; i++){
-            if (k == i)
-                 b = 0;
-            else{
-                if (str[k] == str[i])
+            if ((k != i) &&(str[k] == str[i]))
                     return str[k];
-                else
-                    b = 0;
             }
         }
-    }
-    if (b = -1)
-        return '0';
 }
+
 bool itc_isFirstInSecond(string s1, string s2){
     bool a;
     string st1,st2;
